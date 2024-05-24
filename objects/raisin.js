41f71e04e27efebfcbe355160aaff20bcca26827
Daman Bual <damanbual@gmail.com>
2024-05-23 17:29:10 -0700

    @victor added stretch function, not Michael is going to try it as expected outcome was incorrect

--- objects/raisin.js
+++ objects/raisin.js
@@ -1,5 +1,5 @@
 
-const raisinAlarmArray = function(cookie) {
+const raisinAlarm = function(cookie) {
   for (let i = 0; i < cookie.length; i++) {
     if (cookie[i] === "🍇") {
       return "Raisin alert!";
@@ -8,8 +8,39 @@ const raisinAlarmArray = function(cookie) {
   return "All good!";
 };
 
+const raisinAlarmArray = function (cookies) {
+  // Put your solution here
+  const result = [];
+  for (const biscuit of cookies) {
+    if (biscuit === "🍇") {
+      result.push("Raisin alert!");
+    } else {
+      result.push("All good!");
+    }
+  }
+  return result;
+};
 
 
+
+// Test Code
 console.log(raisinAlarm(["🍫", "🍫", "🍇", "🍫"]));
 console.log(raisinAlarm(["🍫", "🍇", "🍫", "🍫", "🍇"]));
 console.log(raisinAlarm(["🍫", "🍫", "🍫"]));
+
+console.log("------");
+
+console.log(
+  raisinAlarmArray([
+    ["🍫", "🍫", "🍇", "🍫"],
+    ["🍫", "🍇", "🍫", "🍫", "🍇"],
+    ["🍫", "🍫", "🍫"],
+  ])
+);
+
+
+
+
+
+
+
