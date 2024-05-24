7b5933ec358f7ebaf114f4f3559e471a4288db20
Daman Bual <damanbual@gmail.com>
2024-05-23 17:06:14 -0700

    added test start code, @@VictorDQuintero now is coder and I navigate

--- objects/raisin.js
+++ objects/raisin.js
@@ -1,6 +1,6 @@
 
-const raisinAlarmArray = function (cookie) {
-  for (let i = 0; i < cookie.length; i++ ) {
+const raisinAlarmArray = function(cookie) {
+  for (let i = 0; i < cookie.length; i++) {
     if (cookie[i] === "🍇") {
       return "Raisin alert!";
     }
@@ -10,10 +10,6 @@ const raisinAlarmArray = function (cookie) {
 
 
 
-console.log(
-  raisinAlarmArray([
-    ["🍫", "🍫", "🍇", "🍫"],
-    ["🍫", "🍇", "🍫", "🍫", "🍇"],
-    ["🍫", "🍫", "🍫"],
-  ])
-);
+console.log(raisinAlarm(["🍫", "🍫", "🍇", "🍫"]));
+console.log(raisinAlarm(["🍫", "🍇", "🍫", "🍫", "🍇"]));
+console.log(raisinAlarm(["🍫", "🍫", "🍫"]));
