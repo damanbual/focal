fd2d75b3c6b8d6dfd7893df8eab92802c7863184
Daman Bual <damanbual@gmail.com>
2024-05-23 17:00:16 -0700

    added function and @@VictorDQuintero was the navigator

--- objects/raisin.js
+++ objects/raisin.js
@@ -1,8 +1,13 @@
 
-const raisinAlarmArray = function (cookies) {
-
-
+const raisinAlarmArray = function (cookie) {
+  for (let i = 0; i < cookie.length; i++ ) {
+    if (cookie[i] === "🍇") {
+      return "Raisin alert!";
+    }
+  }
+  return "All good!";
 };
+
 
 
 console.log(
