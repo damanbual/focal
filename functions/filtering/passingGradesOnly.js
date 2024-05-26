8af6e59249c3c7f6db640a74dec5381ba14a582a
Daman Bual <damanbual@gmail.com>
2024-05-26 09:14:40 -0700

    completed code

--- functions/filtering/passingGradesOnly.js
+++ functions/filtering/passingGradesOnly.js
@@ -12,3 +12,8 @@ console.log("Passing grades:", passingGrades);
 const passingGrades = grades.filter(grade => grade >= 70);
 console.log("Passing grades:", passingGrades);
 
+// Completed code 
+
+const grades = [73, 69, 3, 100, 50, 70, 69, 88, 95, 77, 35];
+const passingGrades = grades.filter(grade => grade >= 70);
+console.log("Passing grades:", passingGrades);
