58b9f18ff5b4f8708f614505255ddda5dfc310a8
Daman Bual <damanbual@gmail.com>
2024-05-26 08:57:09 -0700

    added anonymous function instead

--- functions/callbacks-intro/findWaldoAnonymously.js
+++ functions/callbacks-intro/findWaldoAnonymously.js
@@ -8,8 +8,8 @@ const findWaldo = function(names, found) {
   });
 };
 
-const actionWhenFound = function(index) {
-  console.log(`Found Waldo at index ${index}!`);
-};
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], function(index) {
+  console.log(`Found Waldo at indext ${index}!`);
+});
