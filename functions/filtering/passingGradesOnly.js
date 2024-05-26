c17724c575efbc913fb852c031c3ff109a873296
Daman Bual <damanbual@gmail.com>
2024-05-26 09:11:15 -0700

    added refactoring the callback itno inline and anon function

---
+++ functions/filtering/passingGradesOnly.js
@@ -0,0 +1,10 @@
+
+const grades = [73, 69, 3, 100, 50, 70, 69, 88, 95, 77, 35];
+// Write code to filter this list down to only passing grades.
+
+const passingGrades = grades.filter(function(grade) {
+  return frade >= 70;
+});
+
+console.log("Passing grades:", passingGrades);
+
